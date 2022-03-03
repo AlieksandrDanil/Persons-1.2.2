b13008e029cfed7a5232062331bf31d843d6a368
AlieksandrDanil <inspcrimea@gmail.com>
2022-03-03 16:16:08 +0300

    Added code in MainKt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
-
 fun main() {
-    println("Run project Persons")
+    val likes = 61
+    val lastFigure: Char = likes.toString().last()
+    val end = if (likes == 0 || likes in 2..20 || lastFigure.digitToInt() != 1) {
+        "ам"
+    } else if (lastFigure.digitToInt() == 1) {
+        "у"
+    } else {
+        ""
+    }
+    println("Понравилось $likes человек$end")
 }
