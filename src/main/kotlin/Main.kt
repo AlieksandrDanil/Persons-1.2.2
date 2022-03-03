6f3d7233e2cdedc82be5241717ce0eee43f3b11e
AlieksandrDanil <inspcrimea@gmail.com>
2022-03-03 22:07:38 +0300

    Corrected val Persons

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 fun main() {
-    val likes = 61
+    print("Введите целым числом количество лиц, которым понравилось данное видео: ")
+    val likes = readLine()?.toInt() ?: return
     val lastFigure: Char = likes.toString().last()
-    val end = if (likes == 0 || likes in 2..20 || lastFigure.digitToInt() != 1) {
-        "ам"
+    val persons = if (likes == 0 || likes in 2..20 || lastFigure.digitToInt() != 1) {
+        "людям"
     } else if (lastFigure.digitToInt() == 1) {
-        "у"
+        "человеку"
     } else {
         ""
     }
-    println("Понравилось $likes человек$end")
+    println("Понравилось $likes $persons")
 }
